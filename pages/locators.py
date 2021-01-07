00f1fb342c6802642b02465cde81edeef908a07d
VS <mixworldapp@gmail.com>
2021-01-07 19:06:39 +0600

    add tests, locators, class of LoginPage

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '#login_form > button')
+    #register form
+    REGISTER_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_PASSWORD_CONFIRM = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '#register_form > button')
