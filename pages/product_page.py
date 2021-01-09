2849cdb1e32c9585a10d1ae2b1970caf016abebb
VS <mixworldapp@gmail.com>
2021-01-09 21:10:08 +0600

    add negative wait methods, add new test for product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,6 +12,18 @@ class ProductPage(BasePage):
         name_product = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         assert name_product == name_product_in_message, f'Expect {name_product} in success message, got {name_product_in_message}'
 
+    def should_not_be_add_to_basket_message(self):
+        check_success_message = self.is_not_element_present(*ProductPageLocators.PRODUCT_ADD_BASKET_SUCCESS_MESS)
+        assert check_success_message, f'Not expect a success message add basket, got a success message'
+
+    def should_not_be_add_to_basket_message_at_product_page(self):
+        check_success_message = self.is_not_element_present(*ProductPageLocators.PRODUCT_ADD_BASKET_SUCCESS_MESS)
+        assert check_success_message, f'Not expect a success message add basket, got a success message'
+
+    def should_disappear_add_to_basket_message(self):
+        disappear_success_message = self.is_disappeared(*ProductPageLocators.PRODUCT_ADD_BASKET_SUCCESS_MESS)
+        assert disappear_success_message, f'Expect disappear message add basket, got not disappear message'
+
     def should_be_product_cost_equal_basket_cost(self):
         basket_cost_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_ADD_BASKET_COST_MESS).text
         product_cost = self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text
--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,9 @@ from .pages.product_page import ProductPage
 import pytest
 
 
-#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+link_product_page = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
+@pytest.mark.skip(reason="no way of currently testing this")
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -15,10 +16,28 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    print(link)
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_add_to_basket_message()
     page.should_be_product_cost_equal_basket_cost()
+
+@pytest.mark.xfail(reason="known message issue")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_product_page)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_add_to_basket_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link_product_page)
+    page.open()
+    page.should_not_be_add_to_basket_message_at_product_page()
+
+@pytest.mark.xfail(reason="known message disappear issue")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_product_page)
+    page.open()
+    page.add_product_to_basket()
+    page.should_disappear_add_to_basket_message()
