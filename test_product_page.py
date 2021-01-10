892d271e485032bee58a5bad3b5bb26f5a8c8758
VS <mixworldapp@gmail.com>
2021-01-10 16:44:44 +0600

    add tests for register user interact with basket, group to class

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import pytest
 
 
 link_product_page = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+link_user_product_page = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 @pytest.mark.skip(reason="no way of currently testing this")
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -44,21 +47,44 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_disappear_add_to_basket_message()
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_basket_empty()
+
+@pytest.mark.user_work_with_basket
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        #prepare test data
+        email_test_data = page.generate_random_string(8) + '@random.org'
+        password_test_data = page.generate_random_string(10)
+        #setup step
+        page.open()
+        page.register_new_user(email_test_data, password_test_data)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link_user_product_page)
+        page.open()
+        page.add_product_to_basket()
+        page.should_be_add_to_basket_message()
+        page.should_be_product_cost_equal_basket_cost()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link_user_product_page)
+        page.open()
+        page.should_not_be_add_to_basket_message_at_product_page()
